257caa70e33ae7c7eb62f0123c8b2f2a58f3732d
Matan Lurey <matan@lurey.org>
2025-01-30 13:19:25 -0800

    Fix, add flutter devices.

--- setup.sh
+++ setup.sh
@@ -39,8 +39,8 @@ git clone --depth 1 "$FLUTTER_ORIGIN" "$FLUTTER_DIR"
 git -C "$FLUTTER_DIR" remote add upstream https://github.com/flutter/flutter.git
 
 # Checkout the Flutter version we need.
-git -C "$FLUTTER_DIR" fetch --no-tags --prune --depth=1 origin "$FLUTTER_SHA"
-git -C "$FLUTTER_DIR" fetch --no-tags --prune --depth=1 origin master
+git -C "$FLUTTER_DIR" fetch --no-tags --prune --depth=1 upstream master
+git -C "$FLUTTER_DIR" checkout "$FLUTTER_SHA"
 
 # Set the engine prebuilt.
 export FLUTTER_PREBUILT_ENGINE_VERSION="$FLUTTER_SHA"
